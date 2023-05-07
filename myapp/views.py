6fa72388e03bc2917b246a03f0bb6b0c85758096
“Gjorgi <georgiedelev@gmail.com>
2023-05-07 21:03:54 +0200

    get all options that the user posted or view them genreally

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
 # Create your views here.
 
 def index(request):
-    foods = Food.objects.all()
+    if request.method == "POST":
+                                # Whatever the user selected only name
+        food_consumed = request.POST['food_consumed']
+        # for whole food object
+        consume = Food.objects.get(name=food_consumed)
+        # current user
+        user = request.user
+        consume = Consume(user=user, food_consumed=consume)
+        consume.save()
+        foods = Food.objects.all()
+
+    else:
+        foods = Food.objects.all()
     return render(request, 'myapp/index.html', {'foods':foods})
 
