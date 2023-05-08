cbb2f77b29d1cf6a52ed3c5772d92c6e7a1dbfd3
“Gjorgi <georgiedelev@gmail.com>
2023-05-08 13:28:19 +0200

    fixing auto adding the last item added on refresh

--- myapp/views.py
+++ myapp/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, redirect
 from .models import Food, Consume
 # Create your views here.
 
+
+from django.urls import reverse
+from .models import Food, Consume
+
 def index(request):
     if request.method == "POST":
-                                # Whatever the user selected only name
-        food_consumed = request.POST['food_consumed']
-        # for whole food object
-        consume = Food.objects.get(name=food_consumed)
-        # current user
-        user = request.user
-        consume = Consume(user=user, food_consumed=consume)
-        consume.save()
-        foods = Food.objects.all()
+        food_consumed = request.POST.get('food_consumed')  # use get() instead of [] to avoid KeyError
+        if food_consumed:
+            consume = Food.objects.get(name=food_consumed)
+            user = request.user
+            Consume.objects.create(user=user, food_consumed=consume)
+            # redirect to the same page to prevent form resubmission
+            return redirect(reverse('index'))
 
-    else:
-        foods = Food.objects.all()
+    foods = Food.objects.all()
     consumed_food = Consume.objects.filter(user=request.user)
     return render(request, 'myapp/index.html', {'foods':foods, 'consumed_food': consumed_food})
-
